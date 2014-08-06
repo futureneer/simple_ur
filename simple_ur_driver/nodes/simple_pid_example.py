a534e5747395aea8f5ecb4f0a34fa85d71e987b9
Kel Guerin <futureneer@gmail.com>
2014-08-06 11:26:15 -0400

    simple working speedl example

--- simple_ur_driver/nodes/simple_pid_example.py
+++ simple_ur_driver/nodes/simple_pid_example.py
@@ -97,18 +97,21 @@ def check_distance(a,b,val):
 if __name__ == "__main__":
 
 
-    # rob = urx.Robot("192.168.1.155", logLevel=logging.INFO)
+
+    rob = urx.Robot("192.168.1.155", logLevel=logging.INFO)
+    print 'connected to robot'
     sock = socket.create_connection(('192.168.1.155', 30003), timeout=0.5)
     print 'created socket'
-    # rob.set_tcp((0,0,0,0,0,0))
-    # rob.set_payload(0.5, (0,0,0))
+    if rob:
+        realtime_monitor = rob.get_realtime_monitor()
+        print 'created realtime monitor connection'
     rospy.sleep(1)
 
-    for n in range(500):
-        vels = [0.0, 0.0, -0.1, 0.0, 0.0, 0.0]
+    for n in range(100):
+        vels = [0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
         acc = .1
         vels.append(acc)
-        timeout = .008
+        timeout = .05
         vels.append(timeout)
         prog = "speedl([{},{},{},{},{},{}], a={}, t_min={})\n".format(*vels)
         # prog = "textmsg({})\n".format(n)
@@ -116,10 +119,13 @@ if __name__ == "__main__":
             prog = prog.encode()
         print 'sending command ['+str(prog)+']'
         sock.send(prog)
-        rospy.sleep(.007)
+        # print realtime_monitor.get_all_data()['tcp'][2]
+        rospy.sleep(.045)
+        # sock.recv(1044)
 
     rospy.sleep(1)
     sock.close() 
+    rob.cleanup()
     # P = 1.0
     # I = 0.0
     # D = 0.1
