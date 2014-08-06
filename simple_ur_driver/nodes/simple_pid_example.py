03a62f9d9e8e0ebbe976c2a2690370577a044d22
Kel Guerin <futureneer@gmail.com>
2014-08-06 11:58:25 -0400

    rt 30003 interface working example

--- simple_ur_driver/nodes/simple_pid_example.py
+++ simple_ur_driver/nodes/simple_pid_example.py
@@ -107,21 +107,21 @@ if __name__ == "__main__":
         print 'created realtime monitor connection'
     rospy.sleep(1)
 
-    for n in range(100):
-        vels = [0.0, 0.0, -0.1, 0.0, 0.0, 0.0]
+    for n in range(500):
+        vels = [0.0, 0.0, 0.05, 0.0, 0.0, 0.0]
         acc = .1
         vels.append(acc)
-        timeout = .05
+        timeout = .01
         vels.append(timeout)
         prog = "speedl([{},{},{},{},{},{}], a={}, t_min={})\n".format(*vels)
         # prog = "textmsg({})\n".format(n)
         if type(prog) != bytes:
             prog = prog.encode()
-        print 'sending command ['+str(prog)+']'
+        # print 'sending command ['+str(prog)+']'
         sock.send(prog)
         print realtime_monitor.get_all_data(wait=False)['tcp'][2]
         # sock.recv(1044)
-        rospy.sleep(.045)
+        rospy.sleep(.007)
 
     rospy.sleep(1)
     sock.close() 
