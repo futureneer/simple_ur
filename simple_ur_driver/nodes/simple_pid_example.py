66345febcb870fc34e06ff1dc12155282aae3471
Kel Guerin <futureneer@gmail.com>
2014-08-06 13:36:15 -0400

    working pid with speedl commands

--- simple_ur_driver/nodes/simple_pid_example.py
+++ simple_ur_driver/nodes/simple_pid_example.py
@@ -88,7 +88,7 @@ def check_distance(a,b,val):
     v1 = np.array(a)
     v2 = np.array(b)
     res = np.sum(np.abs(np.subtract(v1,v2)))
-    # rospy.logwarn(res)
+    # print res
     if res < val:
         return True
     else:
@@ -107,90 +107,96 @@ if __name__ == "__main__":
         print 'created realtime monitor connection'
     rospy.sleep(1)
 
-    for n in range(500):
-        vels = [0.0, 0.0, 0.05, 0.0, 0.0, 0.0]
-        acc = .1
-        vels.append(acc)
-        timeout = .01
-        vels.append(timeout)
-        prog = "speedl([{},{},{},{},{},{}], a={}, t_min={})\n".format(*vels)
-        # prog = "textmsg({})\n".format(n)
+    # for n in range(500):
+    #     vels = [0.0, 0.0, 0.05, 0.0, 0.0, 0.0]
+    #     acc = .1
+    #     vels.append(acc)
+    #     timeout = .01
+    #     vels.append(timeout)
+    #     prog = "speedl([{},{},{},{},{},{}], a={}, t_min={})\n".format(*vels)
+    #     # prog = "textmsg({})\n".format(n)
+    #     if type(prog) != bytes:
+    #         prog = prog.encode()
+    #     # print 'sending command ['+str(prog)+']'
+    #     sock.send(prog)
+    #     print realtime_monitor.get_all_data(wait=False)['tcp'][2]
+    #     # sock.recv(1044)
+    #     rospy.sleep(.007)
+
+    # rospy.sleep(1)
+    # sock.close() 
+    # rob.cleanup()
+
+
+    P = 10.0
+    I = 0.0
+    D = 0.1
+
+    pids = []
+    for i in range(6):
+        pids.append(PID(P,I,D))
+
+    ### Get init pose
+    # start_pose = current_pose = rob.getl()
+    start_pose = current_pose = realtime_monitor.get_all_data(wait=False)['tcp']
+    # start_pose = [1.0, 1.0, 1.0, 1.507, 0.0, -1.507]
+    # current_pose = [1.0, 1.0, 1.0, 1.507, 0.0, -1.507]
+
+    ### Create Command Pose
+    command_pose = list(start_pose)
+    command_pose[2] = command_pose[2] + .2
+
+    for p, i in zip(pids, range(6)):
+        p.setPoint(command_pose[i])
+
+    print 'Start Pose: '+ str(start_pose)
+    print 'End Pose; '+str(command_pose)
+
+    vel_command_last = []
+        
+    acc = .1
+    timeout = .01
+
+    while not check_distance(command_pose,current_pose,.001):
+        # Get current values
+        current_pose = realtime_monitor.get_all_data(wait=False)['tcp']
+        # print current_pose
+        vel_command = []
+        for p, i in zip(pids, range(6)):
+            up = p.update(current_pose[i])
+            vel_command.append(up)
+
+        V = []
+        for v,i in zip(vel_command,range(6)):
+            if v > 0:
+                if v > 1.5:
+                    vel_command[i] = 1.5
+            else:
+                if v < -1.5:
+                    vel_command[i] = -1.5
+        vel_command_last = list(vel_command)
+        # print vel_command[2]
+        vel_command.append(acc)
+        vel_command.append(timeout)
+        prog = "speedl([{},{},{},{},{},{}], a={}, t_min={})\n".format(*vel_command)
         if type(prog) != bytes:
             prog = prog.encode()
         # print 'sending command ['+str(prog)+']'
         sock.send(prog)
-        print realtime_monitor.get_all_data(wait=False)['tcp'][2]
-        # sock.recv(1044)
-        rospy.sleep(.007)
 
-    rospy.sleep(1)
-    sock.close() 
-    rob.cleanup()
-
-
-    # P = 1.0
-    # I = 0.0
-    # D = 0.1
-
-    # pids = []
-    # for i in range(6):
-    #     pids.append(PID(P,I,D))
-
-    # ### Get init pose
-    # start_pose = current_pose = rob.getl()
-    # # start_pose = [1.0, 1.0, 1.0, 1.507, 0.0, -1.507]
-    # # current_pose = [1.0, 1.0, 1.0, 1.507, 0.0, -1.507]
-
-    # ### Create Command Pose
-    # command_pose = list(start_pose)
-    # command_pose[2] = command_pose[2] - .2
-
-    # for p, i in zip(pids, range(6)):
-    #     p.setPoint(command_pose[i])
-
-    # print 'Start Pose: '+ str(start_pose)
-    # print 'End Pose; '+str(command_pose)
-
-    # vel_command_last = []
-    # # rob.movel(command_pose,.1,.06)
-    # # rob.speedl([0,0,.1,0,0,0],.1,1)
-
-    # while not check_distance(command_pose,current_pose,.001):
-    #     # Get current values
-    #     current_pose = rob.getl()
-
-    #     vel_command = []
-    #     for p, i in zip(pids, range(6)):
-    #         up = p.update(current_pose[i])
-    #         vel_command.append(up)
-
-    #     V = []
-    #     for v,i in zip(vel_command,range(6)):
-    #         if v > 0:
-    #             if v > 1.5:
-    #                 vel_command[i] = 1.5
-    #         else:
-    #             if v < -1.5:
-    #                 vel_command[i] = -1.5
-
-    #     vel_command_last = list(vel_command)
-
-    #     rob.speedl(velocities=vel_command, acc=.5, min_time=0.1, direct=True)
+        # rob.speedl(velocities=vel_command, acc=.5, min_time=0.1, direct=True)
         
-    #     # print vel_command
-    #     # print current_pose
-    #     # result = []
-    #     # for c,v in zip(current_pose,vel_command):
-    #         # result.append(c+v)
-    #     # print result
-    #     # current_pose = list(result)
+        # print vel_command
+        # print current_pose
+        # result = []
+        # for c,v in zip(current_pose,vel_command):
+            # result.append(c+v)
+        # print result
+        # current_pose = list(result)
 
-    #     rospy.sleep(.1)
+        rospy.sleep(.005)
 
-    # rob.stopl(.1)
-
-
-    # rob.cleanup()
+    rob.cleanup()
 
 
 
